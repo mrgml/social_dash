66c9343df80d8490778387e76adde2b375d5326b
Gary Lyons <mrgml@yahoo.com>
2012-02-14 19:08:02 +0000

    adding initial rails setup

---
+++ guess_the_footballer/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GuessTheFootballer::Application.config.session_store :cookie_store, key: '_guess_the_footballer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GuessTheFootballer::Application.config.session_store :active_record_store
