ea140bddea764c909501a06a185e5c213079feed
Gary Lyons <mrgml@yahoo.com>
2012-02-16 20:37:47 +0000

    fix failing test

--- guess_the_footballer/features/step_definitions/user_steps.rb
+++ guess_the_footballer/features/step_definitions/user_steps.rb
@@ -6,10 +6,10 @@ Then /^there should be a text field$/ do
   step %Q{I should see "" within "input[id=user_username]"}
 end
 
-When /^I submit the username "([^"]*)"$/ do |arg1|
-  @username = arg1
+When /^I submit the username "([^"]*)"$/ do |arg|
+  @username = arg
   fill_in "user_username", :with => @username
-  click_button "Play!"
+  click_button "Play >>"
 end
 
 Then /^a new user should be created with that username$/ do
