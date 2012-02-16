03189f3c16eefb54e7e6d64b3617c56726be8b15
Gary Lyons <mrgml@yahoo.com>
2012-02-16 14:58:25 +0000

    minor tidy
    	modified:   features/step_definitions/guess_steps.rb

--- guess_the_footballer/features/step_definitions/guess_steps.rb
+++ guess_the_footballer/features/step_definitions/guess_steps.rb
@@ -3,8 +3,8 @@ Given /^I browse to the biography guess page for "([^"]*)"$/ do |name|
   visit biography_path(@biography.id)
 end
 
-
+# TODO: find out why a string with /n's in it makes cucumber think there is a new step  
 Then /^I should see his redacted biography$/ do
-  redacted_biography = @biography.redacted_bio.slice(0,99) # TODO: find out why a string with /n's in it makes cucumber think there is a new step
+  redacted_biography = @biography.redacted_bio.slice(0,99) 
   step %Q{I should see "#{redacted_biography}" within "div.bio/p"}
 end
