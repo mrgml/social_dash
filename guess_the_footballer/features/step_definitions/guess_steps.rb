d0c4fe3955ffe42923e13bb57f1126a6cc73f9aa
Gary Lyons <mrgml@yahoo.com>
2012-02-16 14:55:51 +0000

    changing stylesheet inclusion order updating test for finer detail updating view to output more markup for styling

--- guess_the_footballer/features/step_definitions/guess_steps.rb
+++ guess_the_footballer/features/step_definitions/guess_steps.rb
@@ -6,5 +6,5 @@ end
 
 Then /^I should see his redacted biography$/ do
   redacted_biography = @biography.redacted_bio.slice(0,99) # TODO: find out why a string with /n's in it makes cucumber think there is a new step
-  step %Q{I should see "#{redacted_biography}" within "p"}
+  step %Q{I should see "#{redacted_biography}" within "div.bio/p"}
 end
