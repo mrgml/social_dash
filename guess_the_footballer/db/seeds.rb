d9675f892d93a13b1f05fe37b5b04383ee0f2b4b
Gary Lyons <mrgml@yahoo.com>
2012-02-16 09:27:50 +0000

    configuring utf-8 and seeding database
    	modified:   config/database.yml
    	modified:   db/seeds.rb

--- guess_the_footballer/db/seeds.rb
+++ guess_the_footballer/db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Biography.create!(name: 'John Barnes', full_name: 'John Charles Bryan Barnes', bio: "John Charles Bryan Barnes MBE (born 7 November 1963) is a Jamaican-born English football manager, rap artist and former player.
+During his playing career, Barnes had successful periods at Watford and Liverpool in the 1980s and 1990s, winning the First Division twice, the FA Cup twice, and playing for England 79 times (a former record for a black player). After a period at Newcastle United, he ended his playing career at Charlton Athletic in 1999.
+After retiring as a player, Barnes had a brief stint as head coach of Celtic in 1999–2000, working alongside Kenny Dalglish. He took over as manager of the Jamaica national team on 1 November 2008, but resigned six months later to become manager at Tranmere Rovers. He was sacked by Tranmere on 9 October 2009 after a series of poor results.", redacted_bio: "This player, MBE (born 7 November 1963) is a Jamaican-born English football manager, rap artist and former player.
+During his playing career, this player had successful periods at Watford and Liverpool in the 1980s and 1990s, winning the First Division twice, the FA Cup twice, and playing for England 79 times (a former record for a black player). After a period at Newcastle United, he ended his playing career at Charlton Athletic in 1999.
+After retiring as a player, this player had a brief stint as head coach of Celtic in 1999–2000, working alongside Kenny Dalglish. He took over as manager of the Jamaica national team on 1 November 2008, but resigned six months later to become manager at Tranmere Rovers. He was sacked by Tranmere on 9 October 2009 after a series of poor results.")
